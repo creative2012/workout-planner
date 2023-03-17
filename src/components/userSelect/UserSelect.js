44f0a0d475db02a961f5b9471d0f68563175f15f
Paul Morris <creative2012@hotmail.com>
2023-03-17 20:40:42 +0000

    added framer motion with animation to splash page and header

--- src/components/userSelect/UserSelect.js
+++ src/components/userSelect/UserSelect.js
@@ -1,5 +1,5 @@
 import React, { useState, createRef } from 'react';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './userSelect.css';
 import { getFromLocalStorage } from '../utils/LocalStorage';
 import Button from '@mui/material/Button';
@@ -9,6 +9,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { motion } from "framer-motion";
 
 
 
@@ -31,7 +32,7 @@ const UserSelect = (props) => {
     const userLogin = () => {
         let loginName = textInput.current.value;
         //check user name is longer than 3 chars
-        if(loginName.length >= 3){
+        if (loginName.length >= 3) {
             props.handleLogin(textInput.current.value)
             navigate("/home");
         }
@@ -45,66 +46,70 @@ const UserSelect = (props) => {
     }
 
     return (
-        <div id="splashBg" >
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Create Profile</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        To Begin using our website, please enter your Name below. 
-                    </DialogContentText>
-                    <TextField
-                        required= {true}
-                        inputRef={textInput}
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Name"
-                        type="name"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={userLogin}>Login</Button>
-                </DialogActions>
-            </Dialog>
-            <div id="splash">
-                <div id="header">
-                    <div id="headTitle">
-                        <em>FIT<span className="yellowFont">PLAN</span></em>
-                    </div>
-                    <div>THE BODY
-                        <span className="yellowFont">ACHIVES</span>
-                        WHAT THE MIND
-                        <span className="yellowFont">BELIVES</span>
-                    </div>
-                </div>
-                <div id="title">
-                {test == null ? 'Create new Profile' : ''}
-                </div>
-                <div id="newUserBtn" onClick={test == null ? handleClickOpen : login}>{test == null ? '+' : props.name}</div>
-                <div id="footer">
-                    <div id="featureCont">
-                        <div id="large">
-                            <em>1<span className="yellowFont">K</span>+</em>
-                            <div>Exercises</div>
+        <motion.div
+        initial={{opacity: 1}}
+        exit={{opacity: 0, transition:{duration: 0.5}}}>
+            <div id="splashBg" >
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Create Profile</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            To Begin using our website, please enter your Name below.
+                        </DialogContentText>
+                        <TextField
+                            required={true}
+                            inputRef={textInput}
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Name"
+                            type="name"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={userLogin}>Login</Button>
+                    </DialogActions>
+                </Dialog>
+                <div id="splash">
+                    <div id="header">
+                        <div id="headTitle">
+                            <em>FIT<span className="yellowFont">PLAN</span></em>
                         </div>
-                        <div id="large">
-                            <em>F<span className="yellowFont">i</span>T </em>
-                            <div>Planner App</div>
-                        </div>
-                        <div id="large">
-                            <em>BMI</em>
-                            <div>Check your Fitness</div>
+                        <div>THE BODY
+                            <span className="yellowFont">ACHIVES</span>
+                            WHAT THE MIND
+                            <span className="yellowFont">BELIVES</span>
                         </div>
                     </div>
-                    Get access to a comprehensive list of thousands of exercises, targeting every major muscle group. As
-                    well as our Fitness planner app, which includes a Diet plan.<br /> <br />Copyright © 2023 FitPlan All
-                    Rights Reserved.
+                    <div id="title">
+                        {test == null ? 'Create new Profile' : ''}
+                    </div>
+                    <div id="newUserBtn" onClick={test == null ? handleClickOpen : login}>{test == null ? '+' : props.name}</div>
+                    <div id="footer">
+                        <div id="featureCont">
+                            <div id="large">
+                                <em>1<span className="yellowFont">K</span>+</em>
+                                <div>Exercises</div>
+                            </div>
+                            <div id="large">
+                                <em>F<span className="yellowFont">i</span>T </em>
+                                <div>Planner App</div>
+                            </div>
+                            <div id="large">
+                                <em>BMI</em>
+                                <div>Check your Fitness</div>
+                            </div>
+                        </div>
+                        Get access to a comprehensive list of thousands of exercises, targeting every major muscle group. As
+                        well as our Fitness planner app, which includes a Diet plan.<br /> <br />Copyright © 2023 FitPlan All
+                        Rights Reserved.
+                    </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
