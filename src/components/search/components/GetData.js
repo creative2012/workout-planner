d4c342793ff5bb2c186faa2090aaa9cd95931f54
onanike obinna <onanikefrancis@gmail.com>
2023-03-18 18:42:16 +0000

    added_function_take_of_the_api

--- src/components/search/components/GetData.js
+++ src/components/search/components/GetData.js
@@ -2,3 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./getdata.css";
 import useApi from "../../utils/Api.js";
 import { Link } from "react-router-dom";
+
+function GetData(props) {
+    const options = {
+        headers: { 'x-api-key': 'UZjs8Cbxc5w9apyoeQoHkw==WvbVw7sr6wacZCG2' }
+    };
+    const { data, isLoading, error } = useApi('https://api.api-ninjas.com/v1/exercises?muscle=' + props.data, options);
+    if (isLoading) {
+        return <p>Loading....</p>;
+    } else {
+        console.log(data)
